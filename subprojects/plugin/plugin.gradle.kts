026dc1e52c78983ffa3dbcda21aab1453ab21de2
Tomasz Linkowski <t.linkowski@gmail.com>
2019-06-14 09:09:23 +0200

    #3: added proper build configuration + an empty plugin class

--- subprojects/plugin/plugin.gradle.kts
+++ subprojects/plugin/plugin.gradle.kts
@@ -1,12 +1,48 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+  //region MAIN
   kotlin("jvm") version "1.3.30"
+  `java-gradle-plugin`
+  `kotlin-dsl`
+  //endregion
+
+  //region TEST
+  groovy
+  //endregion
 }
+
+//region gradle.properties
+val kordampVersion: String by project
+val spockVersion: String by project
+val groovyVersion: String by project
+//endregion
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "1.8"
 }
+
+repositories {
+  gradlePluginPortal() // for other Gradle plugins
+}
+
 dependencies {
+  //region MAIN
   implementation(kotlin("stdlib-jdk8"))
+  implementation(group = "org.kordamp.gradle", name = "base-gradle-plugin", version = kordampVersion)
+  //endregion
+
+  //region TEST
+  testImplementation(group = "org.spockframework", name = "spock-core", version = spockVersion)
+  testImplementation(group = "org.codehaus.groovy", name = "groovy-all", version = groovyVersion)
+  //endregion
+}
+
+gradlePlugin {
+  plugins {
+    create("TLinkowskiSuperpom") {
+      id = "pl.tlinkowski.tlinkowski-superpom"
+      implementationClass = "pl.tlinkowski.TLinkowskiSuperpomPlugin"
+    }
+  }
 }
---
+++ subprojects/plugin/src/main/kotlin/pl/tlinkowski/TLinkowskiSuperpomPlugin.kt
@@ -0,0 +1,36 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2019 Tomasz Linkowski.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.tlinkowski
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Applies the concept of a Gradle SuperPOM for all projects of Tomasz Linkowski.
+ *
+ * See: [http://andresalmiray.com/the-gradle-superpom/]
+ *
+ * @author Tomasz Linkowski
+ */
+class TLinkowskiSuperpomPlugin : Plugin<Project> {
+
+  override fun apply(project: Project) {
+
+  }
+}
